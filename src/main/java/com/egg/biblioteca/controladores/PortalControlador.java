009b652e7f80c65d939ca8b31838a2d3a52cb4ef
nicolas <nicolaseduardobrito@gmail.com>
2025-03-06 23:10:09 -0300

    login

--- src/main/java/com/egg/biblioteca/controladores/PortalControlador.java
+++ src/main/java/com/egg/biblioteca/controladores/PortalControlador.java
@@ -1,16 +1,50 @@
 package com.egg.biblioteca.controladores;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.egg.biblioteca.excepciones.MiException;
+import com.egg.biblioteca.servicios.UsuarioServicio;
 
 @Controller
 @RequestMapping("/")
 public class PortalControlador {
+
+    @Autowired
+    private UsuarioServicio usuarioServicio;
 
     @GetMapping("/") // Acá es donde realizamos el mapeo
     public String index() {
         return "index.html"; // Acá es que retornamos con el método.
     }
 
+    @GetMapping("/registrar")
+    public String registrar() {
+        return "registro.html";
+    }
+
+    @GetMapping("/login")
+    public String login() {
+        return "login.html";
+    }
+
+    @PostMapping("/registro")
+    public String registro(@RequestParam String nombre, String email, String password, String password2,
+            ModelMap modelo) {
+        try {
+            usuarioServicio.registrar(nombre, email, password, password2);
+            modelo.put("exito", "Usuario registrado correctamente");
+            return "index.html";
+        } catch (MiException e) {
+            modelo.put("error", e.getMessage());
+            modelo.put("nombre", nombre);
+            modelo.put("email", email);
+            return "registro.html";
+        }
+    }
 }
